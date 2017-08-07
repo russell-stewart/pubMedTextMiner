292639caaa5ac085aadb6beee375ff7de2d39840
Russell Stewart <russells98@gmail.com>
2017-08-07 16:08:51 -0600

    start adding NERTagger compatibility

--- pmcTextMiner.py
+++ pmcTextMiner.py
@@ -3,19 +3,25 @@ import unirest
 import lxml
 import sys
 import getopt
+from collections import Counter
+import subprocess
+
+#Update this with the path to NER Tagger!!
+nerPath = '~/Documents/NationalJewish/Seibold/NERTagger/taggers'
 
 #Get parameters from program call
-opts = getopt.getopt(sys.argv[1:] , '' , ['query=' , 'ofile=' , 'email='])
+opts = getopt.getopt(sys.argv[1:] , '' , ['query=' , 'ofilepath=' , 'email='])
 
 for opt , arg in opts[0]:
     if opt == '--query':
         query = arg
-    if opt == '--ofile':
+    if opt == '--ofilepath':
         oFilePath = arg
     if opt == '--email':
         email = arg
 
 #Search PubMed (w/ user specifed query) and extract all resulting article IDs
+print 'Finding relevant articles on PubMed...'
 searchURL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
 searchResponse = unirest.get(
     searchURL,
@@ -28,11 +34,16 @@ searchResponse = unirest.get(
 )
 ids = BeautifulSoup(searchResponse.body , 'lxml').get_text()
 ids = [int(numeric_string) for numeric_string in ids[:ids.find(' ')].splitlines()]
+print '  %d results found.' % len(ids)
 
 #Retrieve the abstracts for all article ids obtained above
+print 'Retrieving article titles and abstracts...'
 getRecordURL = 'https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi'
+titles = ''
 abstracts = ''
-for currentID in ids:
+success = 0
+fail = 0
+for currentID in ids[1:]:
     identifier = 'oai:pubmedcentral.nih.gov:%d' % currentID
     getRecordResponse = unirest.get(
         getRecordURL,
@@ -44,13 +55,39 @@ for currentID in ids:
     )
     #fish the abstract out of the slop of xml returned by the API
     #and append it to the abstracts array
+
     try:
-        abstracts.append(BeautifulSoup(getRecordResponse.body , 'lxml').find('title').get_text())
-        abstracts.append(BeautifulSoup(getRecordResponse.body , 'lxml').find('abstract').get_text())
-        print 'Article %d retreived' % currentID
+        xml = BeautifulSoup(getRecordResponse.body , 'lxml')
+        title = xml.find('title').get_text()
+        abstract = xml.find('abstract').get_text()
+        titles += title + '\n'
+        abstracts += abstract + '\n'
+        success += 1
     except:
         #catches any API response without an abstract
-        print "Uh oh! GETting article %d didn't work" % currentID
-        print getRecordResponse.body
+        fail += 1
 
-#
+print '  %d title/abstract pairs read successfully.' % success
+if fail:
+    print '  %d could not be read.' % fail
+
+#write titles and abstracts to files for use by the named entity
+#recognition pipeline
+
+titlesPath = oFilePath + '/titles.txt'
+titleFile = open(titlesPath , 'w')
+titleFile.write(titles.encode('utf8'))
+titleFile.close()
+abstractsPath = oFilePath + '/abstracts.txt'
+abstractFile = open(abstractsPath , 'w')
+abstractFile.write(abstracts.encode('utf8'))
+abstractFile.close()
+
+#run named entity recognition pipeline on titles and on abstracts
+# print 'Running NER on abstracts...'
+# command = '%s/tagger.py --model models/english/ --input %s --output %s' % (nerPath , abstractsPath , oFilePath + '/titlesOut.txt')
+# returncode = subprocess.call(command)
+# print 'Running NER on titles...'
+# command = '%s/tagger.py --model models/english/ --input %s --output %s' % (nerPath , titlesPath , oFilePath + '/titlesOut.txt')
+# returncode = subprocess.call(command)
+# print 'Done!!'
